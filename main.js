3ebcce8aaf7ae952e090bc17f84704912b52d97f
anton-moskalenko-kh <anton.moskalenko.kh@gmail.com>
2022-05-16 13:08:49 +0300

    homework4_ver.1.1.0

--- main.js
+++ main.js
@@ -1,20 +1,25 @@
 // Задание 1
 function getNumber() {
-      let num = prompt('Введите число больше 100:');
-       for (let i = 0; i < 10; i++) {
-           if (num < 100) {
-               num = prompt('Нужно ввести число больше 100, попробуйте еще раз:');
-           }
-           if (num > 100) {
-               return num;
-           }
-       }
-   }
-const result = console.log(getNumber())
+    let num;
+    for (let i = 0; i < 10; i++) {
+        num = prompt('Введите число больше 100:');
+        if (num > 100) {
+            return console.log(num);
+        } else if (num < 100) {
+            if (i === 9) {
+                console.log(num)
+            } else {
+                continue;
+            }
+        } else {
+            return console.log(num);
+        }
+    }
+}
 
 
 // Задание 2
 function checkAge(age) {
     return (age >=18 && age <=100) ? "Ура" : false;
 }
-const value = console.log(checkAge(42));
+console.log(checkAge(42));
